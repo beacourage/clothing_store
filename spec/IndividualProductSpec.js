e04861ca40cbac046c5353b6f10e3825cff00feb
beatricecourage <bea_courage@hotmail.co.uk>
2019-04-10 15:48:55 +0100

    Initialized product has the correct category, test passing

--- spec/IndividualProductSpec.js
+++ spec/IndividualProductSpec.js
@@ -1,11 +1,15 @@
 describe("Individual Product", () => {
 
   beforeEach(() => {
-    product = new IndividualProduct("Almond Toe Court Shoes, Patent Black", "Women’s Footwear", "£99.00", "5")
+    product = new IndividualProduct("Almond Toe Court Shoes, Patent Black","Women’s Footwear","£99.00","5")
   })
 
-  it ("Product has the correct name", () => {
+  it("Initialized product has the correct name", () => {
     expect(product.name).toEqual("Almond Toe Court Shoes, Patent Black")
+  })
+
+  it("Initialized product has the correct category", () => {
+    expect(product.category).toEqual("Women’s Footwear")
   })
 
 
