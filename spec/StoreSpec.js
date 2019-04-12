8583b128c347a82e1c6ad02736f27e9a76a42573
beatricecourage <bea_courage@hotmail.co.uk>
2019-04-12 17:18:11 +0100

    Can find the name of a particular product, test passing

--- spec/StoreSpec.js
+++ spec/StoreSpec.js
@@ -9,5 +9,12 @@ describe("Store", () => {
     expect(store.products.length).toEqual(13)
   })
 
+  it("Can find the name of a particular product", () => {
+    expect(store.products[7].name).toEqual("Fine Stripe Short Sleeve Shirt, Grey")
+  })
+
+
+
+
 
 })
