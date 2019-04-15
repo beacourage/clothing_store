d8ae028e4cab462adc625816bc09591240b61a34
beatricecourage <bea_courage@hotmail.co.uk>
2019-04-15 19:35:11 +0100

    Refactored inidividual product spec

--- spec/IndividualProductSpec.js
+++ spec/IndividualProductSpec.js
@@ -4,31 +4,36 @@ describe("Individual Product", () => {
     product = new IndividualProduct("Almond Toe Court Shoes, Patent Black","Women’s Footwear","£99.00","5")
   })
 
-  it("Initialized product has the correct name", () => {
-    expect(product.name).toEqual("Almond Toe Court Shoes, Patent Black")
+  describe("Initially:", () => {
+
+    it("Initialized product has the correct name", () => {
+      expect(product.name).toEqual("Almond Toe Court Shoes, Patent Black")
+    })
+
+    it("Initialized product has the correct category", () => {
+      expect(product.category).toEqual("Women’s Footwear")
+    })
+
+    it("Initialized product has the correct price", () => {
+      expect(product.price).toEqual(99)
+    })
+
+    it("Initialized product has the correct quantity", () => {
+      expect(product.quantity).toEqual(5)
+    })
+
   })
 
-  it("Initialized product has the correct category", () => {
-    expect(product.category).toEqual("Women’s Footwear")
+  describe("Out or in stock:", () => {
+
+    it("Can tell whether a product is out of stock", () => {
+      expect(product.outOfStock()).toBe(false)
+    })
+
+    it("Can tell whether a product is in stock", () => {
+       expect(product.inStock()).toBe(true)
+    })
+
   })
-
-  it("Initialized product has the correct price", () => {
-    expect(product.price).toEqual(99)
-  })
-
-  it("Initialized product has the correct quantity", () => {
-    expect(product.quantity).toEqual(5)
-  })
-
-  it("Can tell whether a product is out of stock", () => {
-    expect(product.outOfStock()).toBe(false)
-  })
-
-  it("Can tell whether a product is in stock", () => {
-    expect(product.inStock()).toBe(true)
-  })
-
-
-
 
 })
